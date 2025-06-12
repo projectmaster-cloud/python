688192f1a7626eba97ca91fef775824d3b6759d7
projectmaster-cloud <abhinavman0330@k12.ipsd.org>
2025-06-12 00:40:40 +0000

    changes made to running

--- running.py
+++ running.py
@@ -3,12 +3,9 @@ import os
 
 
 def run():
-    again = ""
-    while "y" not in again.lower():
-
-          if "n" in again.lower():
-              return False
-              
-          again = input("You must enter yes or no to play again or no: ")
-          os.system("clear")
-    return True
+    while True:
+        again = input("Do you want to play again?: ")
+        if "y" in again[0].lower():
+            return True
+        elif "n" in again[0].lower():
+            return False
